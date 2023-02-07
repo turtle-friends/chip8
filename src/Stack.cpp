d6cb8a4e9819e4df1102cb45c49fe97d6dd1ad85
Junior Betancourt <jrbetancourt98@gmail.com>
2023-02-07 18:36:04 -0500

    feat:added Stack.h and Stack.cpp

---
+++ src/Stack.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file Emulator.cpp
+ *
+ * @author Jasmine Aburialeh
+ * @author Junior Betancourt
+ * @author Richard Larancuente
+ * @author William Abreu
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version
+ *
+ * @section DESCRIPTION
+ *
+ * Creates the windows and handles different events
+ *
+ */
+
+#include "Stack.h"
+
+void Stack::Push(uint16_t data)
+{
+    m_top++;
+    m_buf[m_top] = data;
+}
+
+uint16_t Stack::Pop()
+{
+    m_top--;
+    return m_buf[m_top + 1]; 
+}
